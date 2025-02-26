f4d5cd20b8b48a688f11fd68f083e50f456ad12d
jim jones <1197622311@qq.com>
2025-02-26 20:34:10 +0800

    feat:增加顶部搜索框,电商图标与设置图标

---
+++ src/components/FooterShow.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-menu
+    class="Footermenu"
+    mode="horizontal"
+    background-color="white"
+    active-text-color="#ffd04b"
+    @select="handleSelect"
+  >
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-menu-item index="/cart">购物车</el-menu-item>
+    <el-menu-item index="/profile">我的</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "FooterShow",
+  methods: {
+    handleSelect(index) {
+      this.$router.push(index);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.Footermenu {
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  justify-content: space-around;
+  border-top: 1px solid grey;
+}
+</style>
---
+++ src/components/HeaderShow.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <router-link to="/" class="logo">
+        <h1>E-Shop</h1>
+      </router-link>
+    </div>
+    <div class="header-center">
+      <el-input
+        v-model="searchQuery"
+        placeholder="搜索商品"
+        prefix-icon="el-icon-search"
+        @keyup.enter.native="handleSearch"
+      >
+        <el-button
+          slot="append"
+          icon="el-icon-search"
+          @click="handleSearch"
+        ></el-button>
+      </el-input>
+    </div>
+    <div class="header-right">
+      <el-button icon="el-icon-setting" type="text" @click="goToProfile"
+        >设置</el-button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderShow",
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {
+    handleSearch() {
+      // TODO: 实现搜索逻辑
+      console.log("搜索关键词：", this.searchQuery);
+    },
+    goToProfile() {
+      this.$router.push("/profile");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  height: 60px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.header-left {
+  flex: 1;
+}
+
+.header-center {
+  flex: 2;
+  max-width: 500px;
+}
+
+.header-right {
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.logo {
+  text-decoration: none;
+  color: #409eff;
+}
+
+.logo h1 {
+  margin: 0;
+  font-size: 24px;
+}
+</style>
