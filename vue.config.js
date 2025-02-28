c92e2b2359832ee1e5c3d44fcbf6740028890d68
jim jones <1197622311@qq.com>
2025-02-28 16:30:29 +0800

    fix:修改publicPath:/Go_Mall/->./

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/BD_GoMall/",
+  //publicPath: "/BD_GoMall/",
   configureWebpack: {
     // 确保在生产环境中也能正常使用 Mock.js
     devtool: "source-map",
   },
-  //publicPath: "./",
+  publicPath: "./",
   outputDir: "dist",
   assetsDir: "static",
 });
