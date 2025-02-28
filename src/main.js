b48615ef223fedfe9118c8390a0c8952f56348d0
jim jones <1197622311@qq.com>
2025-02-28 12:33:46 +0800

    将mockjs引入生产环境并部署到Github Pages

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from "./router";
 import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-if (process.env.NODE_ENV === "development") {
-  require("../Mock");
+if (process.env.VUE_APP_USE_MOCK === "true") {
+  require("./Mock");
 }
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/BD_GoMall/" // 替换为您的仓库名
+      : "/",
+  configureWebpack: {
+    // 确保在生产环境中也能正常使用 Mock.js
+    devtool: "source-map",
+  },
 });
