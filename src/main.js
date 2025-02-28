03f7cbcfd3c67dd8eea67240c48dd640cfd958a8
jim jones <1197622311@qq.com>
2025-02-28 12:45:27 +0800

    fix:修复action build过程中显示无法找到Mock文件的问题

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 if (process.env.VUE_APP_USE_MOCK === "true") {
-  require("./Mock");
+  require("../Mock");
 }
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
